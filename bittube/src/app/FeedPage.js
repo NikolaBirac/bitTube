480e29857e4f5ae6c074ecd8406d7012b5ab05dd
NikolaBirac <NikolaBirac>
2018-06-21 12:40:49 +0200

    main video

--- bittube/src/app/FeedPage.js
+++ bittube/src/app/FeedPage.js
@@ -1,1 +1,41 @@
 import React from 'react';
+import Video from './Video';
+import videoService from '../services/videoService';
+
+class FeedPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            video: []
+        }
+    }
+
+    loadVideo() {
+        videoService.getVideo('maya').then(video => {
+
+            let videoId = video.items[0].id.videoId;
+            let videoURL = 'https://www.youtube.com/embed/' + videoId;
+
+            this.setState({
+                video: videoURL
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.loadVideo()
+    }
+
+    render() {
+        return (
+
+            <div className='container'>
+                <Video video={this.state.video} />
+            </div>
+
+        )
+    }
+}
+
+export default FeedPage;
